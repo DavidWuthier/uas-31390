a5840d7071f7edccde2f06b3ce7ef84daee2e949
Bisssen <bisssen@gmailcom>
2021-06-02 15:43:59 +0200

    Added scalings and offsets

--- maze/exercise_4_files/map_3d.m
+++ maze/exercise_4_files/map_3d.m
@@ -1,24 +1,24 @@
 close all
 clear all
 clc
-%% Setup the map 
-% Define the map size
-max_x = 4;
-max_y = 5;
-max_z = 3;
-map = zeros(max_x, max_y, max_z);
-
+%% Setup the map
 % Load the map from the wall file. Each line, except the two last, in the
 % wall file is an obstacle. The position of the obstacle is defined in the
-% wall file by its x y z coordinates. The two last elements is the starting
-% position and goal position
+% wall file by its x y z coordinates. The three last elements is
+% the size of the maze, the starting position and goal position
 load('wall.txt')
 % If a different named file is used, then write it into the wall variable
 % e.g. wall = maze_2;
 wall = wall;
 
+% Define the map size
+max_x = wall(length(wall) - 2, 1);
+max_y = wall(length(wall) - 2, 2);
+max_z = wall(length(wall) - 2, 3);
+map = zeros(max_x, max_y, max_z);
+
 % Input the obstacles into the map
-for i = 1:(length(wall) - 2)
+for i = 1:(length(wall) - 3)
     map = gen_square3d([wall(i, 1) wall(i, 1) + 1;...
                         wall(i, 2) wall(i, 2) + 1;...
                         wall(i, 3) wall(i, 3) + 1], map);
@@ -53,7 +53,7 @@ scatter3(end_(1)+0.5, end_(2)+0.5, end_(3)+0.5, ...
 hold on
 
 % Draw the obstacles
-for i = 1:(length(wall) - 2)
+for i = 1:(length(wall) - 3)
     map = gen_square3d([wall(i, 1) wall(i, 1) + 1;...
                         wall(i, 2) wall(i, 2) + 1;...
                         wall(i, 3) wall(i, 3) + 1], map, 1);
@@ -81,3 +81,24 @@ for i = 2:length(route)
     route(i,:)
 end
 hold off
+
+%% Scale the route
+x_scale = 0.65;
+y_scale = 0.55;
+z_scale = 0.75;
+
+x_offset = 0.3;
+y_offset = 0.5;
+z_offset = 0.25;
+
+% Make a copy of the route
+route_scaled = route;
+
+% Scale the copy
+route_scaled(:,1) = (route_scaled(:,1) - 1) * x_scale + x_offset;
+route_scaled(:,2) = (route_scaled(:,2) - 1) * y_scale + y_offset;
+route_scaled(:,3) = (route_scaled(:,3) - 1) * z_scale + z_offset;
+
+% Print the scaled route
+route_scaled
+
