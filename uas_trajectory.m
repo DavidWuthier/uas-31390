0cfe716c7bca0e55ff3fe6e2f37b50be02a50edb
DavidWuthier <dwuthier@gmail.com>
2021-06-03 16:21:20 +0200

    Fix uas_trajectory.m

--- uas_trajectory.m
+++ uas_trajectory.m
@@ -32,17 +32,19 @@ clc
 % Trajectory generation
 
 knots = [0 5];
-order = 8;
 waypoints = cell(1,2);
 waypoints{1} = [0 ; 0 ; 1];
 waypoints{2} = [9 ; 9 ; 1];
-corridors.times = 0.5:0.5:4.5;
-corridors.x_lower = [zeros(1, 4) 8.5 repmat(8.5, 1, 4)];
-corridors.x_upper = [repmat(9, 1, 4) 9.5 repmat(9.5, 1, 4)];
-corridors.y_lower = [repmat(-0.5, 1, 4) -0.5 zeros(1, 4)];
-corridors.y_upper = [repmat(0.5, 1, 4) 0.5 repmat(9, 1, 4)];
-corridors.z_lower = repmat(0.5, 1, 9);
-corridors.z_upper = repmat(1.5, 1, 9);
+% Fix this...
+order = 7;
+corridors.times = [1 4];
+corridors.x_lower = [-1 8];
+corridors.x_upper = [1 10];
+corridors.y_lower = [-1 8];
+corridors.y_upper = [1 10];
+corridors.z_lower = [0 0];
+corridors.z_upper = [2 2];
+% ...until here
 make_plots = true;
 
 poly_traj = uas_minimum_snap(knots, order, waypoints, corridors, make_plots);
