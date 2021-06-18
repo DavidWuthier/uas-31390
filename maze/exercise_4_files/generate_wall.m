f0890bc5010d0d5854b94e24f0e4ffaef1075c46
Bisssen <bisssen@gmailcom>
2021-06-18 11:57:03 +0200

    Final wall generation file and empty wall file

--- maze/exercise_4_files/generate_wall.m
+++ maze/exercise_4_files/generate_wall.m
@@ -21,6 +21,8 @@ end
 
 %first_wall = [1 1 1 1];
 %second_wall = [1 1 1 1];
+first_wall = [0 0 0 0];
+second_wall = [0 0 0 0];
 special_maze = [first_wall second_wall]
 
 
@@ -38,20 +40,20 @@ for y = 1:width
                 continue
             end
             % Create the zigzag pattern
-            if mod(x,2) == 0
+            if mod(x,2) == 1
                 if z == 2 && y == 2
                     map(x, y, z) = 1;
                     fprintf(fileID, '%d %d %d\n', x, y, z);
                 elseif z == 3 && y == 4
                     map(x, y, z) = 1;
                     fprintf(fileID, '%d %d %d\n', x, y, z);
-                elseif z == 2 && y == 4
-                    if (x == 2 && special_maze(5)) || (x == 4 && special_maze(6))
+                elseif z== 2 && y == 4
+                    if (x == 1 && special_maze(5)) || (x == 3 && special_maze(6))
                         map(x, y, z) = 1;
                         fprintf(fileID, '%d %d %d\n', x, y, z);
                     end
                 elseif z == 3 && y == 2
-                    if (x == 2 && special_maze(3)) || (x == 4 && special_maze(4))
+                    if (x == 1 && special_maze(3)) ||(x == 3 && special_maze(4))
                         map(x, y, z) = 1;
                         fprintf(fileID, '%d %d %d\n', x, y, z);
                     end
@@ -64,13 +66,13 @@ for y = 1:width
                 elseif z == 3 && y == 2
                     map(x, y, z) = 1;
                     fprintf(fileID, '%d %d %d\n', x, y, z);
-                elseif z== 2 && y == 2
-                    if (x == 1 && special_maze(1)) || (x == 3 && special_maze(2))
+                elseif z == 2 && y == 2
+                    if (x == 2 && special_maze(1)) || (x == 4 && special_maze(2))
                         map(x, y, z) = 1;
                         fprintf(fileID, '%d %d %d\n', x, y, z);
                     end
                 elseif z == 3 && y == 4
-                    if (x == 1 && special_maze(7)) ||(x == 3 && special_maze(8))
+                    if (x == 2 && special_maze(7)) || (x == 4 && special_maze(8))
                         map(x, y, z) = 1;
                         fprintf(fileID, '%d %d %d\n', x, y, z);
                     end
@@ -82,9 +84,18 @@ end
 
 fprintf(fileID, '\n%s The size, start and goal position\n', '%');
 fprintf(fileID, '%d %d %d\n', depth, width, height);
-fprintf(fileID, '%d %d %d\n', 1, 1, 1);
-fprintf(fileID, '%d %d %d\n', 4, 5, 1);
+
+start_position = randi(4,1,1);
+end_position = randi(4,1,1);
+start_position = 1;
+end_position = 4;
+
+fprintf(fileID, '%d %d %d\n', start_position, 1, 1);
+fprintf(fileID, '%d %d %d\n', end_position, 5, 1);
 fclose(fileID);
+
+start_converter = ['E', 'F', 'G', 'H'];
+end_converter = ['A', 'B', 'C', 'D'];
 
 load('auto_wall.txt')
 % If a different named file is used, then write it into the wall variable
@@ -98,21 +109,30 @@ max_z = wall(length(wall) - 2, 3);
 map = zeros(max_x, max_y, max_z);
 
 figure(1)
+% Mark the start with green
+scatter3(start_position(1)+0.5, 1+0.5, 1+0.5, ...
+         500, [0,1,0],'filled')
+hold on
+
+% Mark the end with red
+scatter3(end_position(1)+0.5, 5+0.5, 1+0.5 + 8, ...
+         500, [1,0,0], 'filled')
 % Draw the obstacles
 for i = 1:(length(wall) - 3)
     if wall(i, 2) == 2
         map = gen_square3d([wall(i, 1) wall(i, 1) + 1;...
                             wall(i, 2) wall(i, 2) + 1;...
-                            wall(i, 3) wall(i, 3) + 1], map, 1);
+                            wall(i, 3) + 1 wall(i, 3) + 2], map, 1);
     else
         map = gen_square3d([wall(i, 1) wall(i, 1) + 1;...
-                            wall(i, 2) - 2 wall(i, 2) + 1 - 2;...
-                            wall(i, 3)+4 wall(i, 3) + 5], map, 1);
+                            wall(i, 2) wall(i, 2) + 1;...
+                            wall(i, 3) + 5 wall(i, 3) + 6], map, 1);
+                        
     end
 end
 
 % Set the axes
-axis([1 max_x+1 1 max_y+1-2 1 max_z+1 + 4])
+axis([1 max_x+1 1 max_y+1 1 max_z+1 + 6])
 % Make the grid lines more visible
 ax = gca;
 ax.GridAlpha = 1.0;
@@ -121,6 +141,9 @@ set(gca, 'xtick', [0:1:max_x])
 set(gca, 'ytick', [0:1:max_y])
 set(gca, 'ztick', [0:1:max_z])
 view(0,0);
+
+fprintf('The drone must start on point: %s and end on point: %s\n', ...
+        start_converter(start_position), end_converter(end_position));
 
 for point = 1:length(special_maze)
     fprintf('Hole: %d = %d\n', point, special_maze(point));
